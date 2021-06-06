04eb3db38b432c0e0e74176f605fb0286cc3c44f
sergiy.belevitnev <sergiy.belevitnev@shutterfly.com>
2021-06-06 12:30:27 +0300

    12 lesson

--- src/lesson12/streamsHomeWork/HoweWorkWithStreams.java
+++ src/lesson12/streamsHomeWork/HoweWorkWithStreams.java
@@ -1,11 +1,15 @@
 package lesson12.streamsHomeWork;
 
+import java.util.List;
 import java.util.Optional;
 
 public class HoweWorkWithStreams {
 
 
     public static void main(String[] args) {
+
+        List<Computer> computerList = ComputerCollection.computersList();
+        computerList.stream().count();
 
     }
 }
